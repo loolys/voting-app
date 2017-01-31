b860219d6a1541c3655224ef16898c6b9d62c13e
loolys <pierre.liljekvist@outlook.com>
2017-01-31 09:22:00 +0100

    Made it so node runs the server

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import Main from './components/main';
+import NotFoundPage from './components/not-found-page';
 
 const routes = (
 	<Route path="/" component={Main}>
-
+		<Route path="*" component={NotFoundPage} />
 	</Route>	
 );
 
